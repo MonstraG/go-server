90dd6cc746fc265a955daf04a9860c290ba7e14c
Arseny Garelyshev <monstrag@gmail.com>
2024-02-17 10:00:39 +0100

    correct mime type for js

--- pages/public.go
+++ pages/public.go
@@ -5,14 +5,20 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func PublicFolderHandler(w http.ResponseWriter, r *http.Request) {
 	var pathValue = r.PathValue("path")
 	path := fmt.Sprintf("public/%s", pathValue)
+
 	content, err := os.ReadFile(path)
 	if err != nil {
 		log.Println("Failed to read file", err)
+	}
+
+	if strings.HasSuffix(path, ".js") {
+		w.Header().Set("Content-Type", "text/javascript; charset=utf-8")
 	}
 	_, err = w.Write(content)
 	if err != nil {
